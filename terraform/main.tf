ebb6fd707a4263387fb1b0b293e4f751002207f0
jeremie de pas <jeremiedepas@wescale.fr>
2026-02-20 17:47:58 +0100

    add pyyaml to nix-shell and fix terraform backend config

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,6 @@ terraform {
     bucket            = "gpu-mig-presentation-tfstate"
     key               = "terraform.tfstate"
     skip_credentials_validation = true
-    skip_metadata_api_ticket     = true
     skip_requesting_account_id  = true
     force_path_style             = true
   }
