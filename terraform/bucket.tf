acac83dced0c2e94daf5a84b49a05c0c387a6848
jeremie de pas <jeremiedepas@wescale.fr>
2026-02-21 10:35:47 +0100

    Enable S3 backend for Terraform state

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -2,8 +2,3 @@ resource "scaleway_object_bucket" "tfstate" {
   name   = "gpu-mig-presentation-tfstate"
   region = var.region
 }
-
-import {
-  id = "fr-par/gpu-mig-presentation-tfstate"
-  to = scaleway_object_bucket.tfstate
-}
