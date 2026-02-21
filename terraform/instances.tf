2d828b78b37ee335e6d4a24120e442bad82745c4
jeremie de pas <jeremiedepas@wescale.fr>
2026-02-21 10:23:56 +0100

    Fix public_ips attribute access (use .address)

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -50,7 +50,7 @@ resource "random_password" "k3s_token" {
 resource "local_file" "k3s_inventory" {
   content  = <<-EOF
     [server]
-    ${scaleway_instance_server.gpu_server.public_ips[0]} k3s_url=https://${scaleway_instance_server.gpu_server.public_ips[0]}:6443 k3s_token=${random_password.k3s_token.result}
+    ${scaleway_instance_server.gpu_server.public_ips[0].address} k3s_url=https://${scaleway_instance_server.gpu_server.public_ips[0].address}:6443 k3s_token=${random_password.k3s_token.result}
 
     [agent]
     
@@ -64,7 +64,7 @@ resource "local_file" "k3s_inventory" {
 resource "local_file" "terraform_outputs" {
   content  = <<-EOF
     # Terraform outputs for GitHub Actions
-    INSTANCE_IP=${scaleway_instance_server.gpu_server.public_ips[0]}
+    INSTANCE_IP=${scaleway_instance_server.gpu_server.public_ips[0].address}
     INSTANCE_ID=${scaleway_instance_server.gpu_server.id}
     K3S_TOKEN=${random_password.k3s_token.result}
     REGION=${var.region}
